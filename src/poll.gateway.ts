14db0a9a50be14384b94b43791856c640ef6ed74
Madandola james <jamesmadandola@gmail.com>
2026-07-15 05:55:49 +0100

    figo

--- src/poll.gateway.ts
+++ src/poll.gateway.ts
@@ -5,9 +5,9 @@ import {
   SubscribeMessage,
   WebSocketServer,
   MessageBody,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   ConnectedSocket,
 } from '@nestjs/websockets';
+import { title } from 'process';
 import { Server, Socket } from 'socket.io';
 
 // @WebSocketGateway opens a socket server on the same port as our app
@@ -18,6 +18,8 @@ export class PollGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server!: Server;
 
+  // Title addition and in memory database
+  private pollTitle: string = 'Favorite Backend Frmawork';
   private pollData: Record<string, number> = {
     NestJS: 0,
     Express: 0,
@@ -25,12 +27,12 @@ export class PollGateway implements OnGatewayConnection, OnGatewayDisconnect {
   };
   // This triggers automatically the moment a client connects
   handleConnection(client: Socket) {
-    console.log(`\n[Server] 🟢 A client connected! Their ID is: ${client.id}`);
+    console.log(`\n[Server]  A client connected! Their ID is: ${client.id}`);
   }
 
   // This triggers automatically when a client closes their app/terminal
   handleDisconnect(client: Socket) {
-    console.log(`\n[Server] 🔴 A client disconnected! ID: ${client.id}`);
+    console.log(`\n[Server]  A client disconnected! ID: ${client.id}`);
   }
 
   // @SubscribeMessage tells Nest to listen for this exact event name
@@ -59,5 +61,45 @@ export class PollGateway implements OnGatewayConnection, OnGatewayDisconnect {
       // send an error only back to the person who made the mistake
       client.emit('ERROR', `Option '${option}' does not exist. `);
     }
+  }
+
+  @SubscribeMessage('ADD_OPTION')
+  handleAddOption(
+    @MessageBody() newOption: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log(
+      `\n[Server]  Request to add new option: "${newOption}" from ${client.id}`,
+    );
+
+    if (this.pollData[newOption] === undefined) {
+      this.pollData[newOption] = 0;
+      // Broadcast title and options
+      this.server.emit('POLL_UPDATE', {
+        title: this.pollTitle,
+        options: this.pollData,
+      });
+    } else {
+      client.emit('ERROR', `The option '${newOption}' already exists!`);
+    }
+  }
+
+  //Listen for create poll
+  @SubscribeMessage('CREATE_POLL')
+  handleCreatePoll(
+    @MessageBody() newTitle: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log(`\n[Server]  ${client.id} created a new poll: "${newTitle}"`);
+
+    //Rest tht poll data and update the title
+    this.pollTitle = newTitle;
+    this.pollData = {};
+
+    // Broadcast new poll to EVERYONE
+    this.server.emit('POLL_UPDATE', {
+      title: this.pollTitle,
+      options: this.pollData,
+    });
   }
 }
