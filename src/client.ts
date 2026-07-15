dd387b1e394fb9c4e89d1fb7c03a4da92644ab85
Madandola james <jamesmadandola@gmail.com>
2026-07-15 18:53:56 +0100

    gio

--- src/client.ts
+++ src/client.ts
@@ -24,20 +24,62 @@ socket.on('connect', () => {
 //NB:completely non-blocking
 //It just wait patiently in the backgroundw until the sever replies
 socket.on('POLL_UPDATE', (data) => {
-  console.log(`\n  ---LIVE POLL: ${data.title.toUpperCase()} ---`);
+  //data now contains titles and options
+  console.log(`\n  --- LIVE POLL: ${data.title.toUpperCase()} ---`);
 
-  const options = Object.entries(data.options));
+  const options = Object.entries(data.options);
 
-  //if there are no options yet , tell the user
-  if (Options.length === 0) {
-     console.log(' (No options yet. Be the first to add one!');
+  if (options.length === 0) {
+    console.log(' (No options et. Be the first to add one!');
   } else {
-
+    // for loop through the option and print them clearly
+    for (const [option, votes] of options) {
+      console.log(` ${option}: ${votes} votes`);
+    }
   }
-  
+
+  console.log('------------------------\n');
+
+  // keep the terminal open and ask for user request
+  rl.question(
+    '  Vote, "add: Option", OR "create: New Poll Topic": ',
+    (answer) => {
+      const input = answer.trim();
+
+      //Parse the command and send the appropriate event to the server
+      if (input.toLowerCase().startsWith('add:')) {
+        // tell the server to add a new option
+        socket.emit('ADD_OPTION', input.substring(4).trim());
+      } else if (input.toLowerCase().startsWith('create:')) {
+        // tell server to create a whole new poll
+        socket.emit('CREATE_POLL', input.substring(7).trim());
+      } else {
+        // just submit as standard vote
+        socket.emit('VOTE', input);
+      }
+    },
+  );
 });
 
+// Listen for errors e.g. sth that does not exist etc.
+socket.on('ERROR', (message) => {
+  console.log(`\n Server Error: ${message}`);
 
+  //keep the prompt going even if they make a mistake so the app doesnt freeze
+  rl.question(
+    ' Try again (Vote, "add: Option", or "create: Topic"): ',
+    (answer) => {
+      const input = answer.trim();
+      if (input.toLowerCase().startsWith('add:')) {
+        socket.emit('ADD_OPTION', input.substring(4).trim());
+      } else if (input.toLowerCase().startsWith('create:')) {
+        socket.emit('CREATE_POLL', input.substring(7).trim());
+      } else {
+        socket.emit('VOTE', input);
+      }
+    },
+  );
+});
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server.');
