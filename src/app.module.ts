5667dbb943fc0deaa77470080941831031a478c8
Madandola james <jamesmadandola@gmail.com>
2026-07-22 05:56:32 +0100

    fixed missing pollservice dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PollGateway } from './poll.gateway';
+import { PollService } from './poll.service';
 
 @Module({
   imports: [],
   controllers: [],
   // We MUST put PollGateway in the providers array, otherwise
   // NestJS will ignore the file completely and the socket won't open.
-  providers: [PollGateway],
+  providers: [PollGateway, PollService],
 })
 export class AppModule {}
